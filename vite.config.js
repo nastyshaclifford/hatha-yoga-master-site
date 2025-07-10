7d7088b52fa0a2fdf1a96b870537213bb22ca1d4
Xeni-ya <budyuginaxeniya@gmail.com>
2025-07-10 16:28:19 +0300

    feat(*): test production build

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,20 @@ import { resolve } from 'path';
 export default defineConfig({
   root: './',
   publicDir: 'assets',
+  base: '/hatha-yoga-master-site/',
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'src/assets/pages/about-me.html'),
+        offer: resolve(__dirname, 'src/assets/pages/offer.html'),
+        policy: resolve(__dirname, 'src/assets/pages/privacy-policy.html')
       }
     }
   },
   server: {
-    port: 3000
+    port: 3000,
+    open: '/index.html'
   }
 });
